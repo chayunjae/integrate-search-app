c541bfc92790ebd8c303874e3c717a854399f291
차윤재 <chayunjae@chayunjaeui-MacBookPro.local>
2024-03-19 19:33:20 +0900

    ver 0.0.1

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
